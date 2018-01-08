2f8f64797d6241ef44f4045155acf2f3cea6ca6a
Tomasz <tomaszp@man.poznan.pl>
2018-01-08 12:53:24 +0100

    Add greedy (not complete)

--- src/main/java/pl/put/poznan/foxtrot/logic/FoxtrotGreedy.java
+++ src/main/java/pl/put/poznan/foxtrot/logic/FoxtrotGreedy.java
@@ -1,10 +1,68 @@
 package pl.put.poznan.foxtrot.logic;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FoxtrotGreedy implements Foxtrot{
+
+
+    private List<Connection> path = new ArrayList();
+    private List<Connection> opened = new ArrayList();
+    private List<Connection> closed = new ArrayList();
+
+    private Connection minConnection;
+
+
+    private Connection getMinConnection() {
+        return minConnection;
+    }
+
+    private boolean setMinConnection(){
+        if (opened != null && !opened.isEmpty()) {
+            Connection best = opened.get(0);
+            for (Connection c : opened) {
+                if( best.getValue() > c.getValue())
+                    minConnection = c;
+            }
+            return true;
+        }
+        else
+            return false;
+    }
+
+    private boolean checkIfExit(Connection con){
+        return con.getTo().getType() == Node.Type.exit;
+    }
+
+    private Path getPath(List<Connection> conList){
+        List<Node> nodeList = new ArrayList<>();
+        float cost = 0;
+        for( Connection c: conList){
+            nodeList.add(c.getFrom());
+            cost += c.getValue();
+        }
+        nodeList.add(conList.get(conList.size()-1).getTo());
+        return  new Path(nodeList,cost);
+    }
     @Override
     public Path find(Graph graph) {
-        return null;
+        Node start = graph.getEntry();
+        Connection minCon;
+        do {
+            opened.addAll(start.getOutgoing());
+            if(setMinConnection()) {
+                minCon = getMinConnection();
+                closed.add(minCon);
+                opened.remove(minCon);
+                start = minCon.getTo();
+            }
+            else {
+                //zglos blad ze nie ma exit w grafie
+                break;
+            }
+
+        } while(!checkIfExit(minCon));
+
+        return getPath(closed); //nie wiem co wrzucic zamiast closed
     }
 }
