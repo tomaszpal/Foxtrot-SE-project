5ebff623c90aa8d0478aa9020f1a1c3e92a8b68b
Makasz <Makaszml1@gmail.com>
2018-01-08 12:09:47 +0100

    Added method to create JSON file.

--- src/main/java/pl/put/poznan/foxtrot/rest/FoxtrotController.java
+++ src/main/java/pl/put/poznan/foxtrot/rest/FoxtrotController.java
@@ -7,7 +7,7 @@ import pl.put.poznan.foxtrot.logic.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
+import java.io.File;
 import java.io.PrintStream;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -22,6 +22,49 @@ public class FoxtrotController {
 
     private static final Logger logger = LoggerFactory.getLogger(FoxtrotController.class);
     Graph lastGraph;
+
+    public void setUp() {
+
+        Graph graph;
+         List<Node> nodeList;
+         List<Connection> connectionList;
+
+        nodeList = new ArrayList<>();
+        connectionList = new ArrayList<>();
+
+        Node n1 = new Node(0, Node.Type.entry, "name1");
+        Node n2 = new Node(1, Node.Type.regular, "name2");
+        Node n3 = new Node(2, Node.Type.regular, "name3");
+        Node n4 = new Node(3, Node.Type.regular, "name4");
+        Node n5 = new Node(4, Node.Type.exit, "name5");
+
+        Connection c1 = new Connection(n1, n2, 1.0f);
+        Connection c2 = new Connection(n1, n3, 3.0f);
+        Connection c3 = new Connection(n2, n4, 2.0f);
+        Connection c4 = new Connection(n3, n5, 4.0f);
+        Connection c5 = new Connection(n4, n5, 5.0f);
+
+
+        nodeList.add(n1);
+        nodeList.add(n2);
+        nodeList.add(n3);
+        nodeList.add(n4);
+        nodeList.add(n5);
+
+        connectionList.add(c1);
+        connectionList.add(c2);
+        connectionList.add(c3);
+        connectionList.add(c4);
+        connectionList.add(c5);
+
+        graph = new Graph(nodeList, connectionList);
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {mapper.writeValue(new File("file.json"), graph);}
+        catch (Exception e){
+
+        }
+    }
 
     //Response to GET request
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
@@ -54,6 +97,7 @@ public class FoxtrotController {
         Path result = null;
         lastGraph = graph;
         System.out.println(transforms);
+        setUp();
         try{
             if(transforms.equals("bfs")){
                 System.out.println("Using BFS");
