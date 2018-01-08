66df64bda8597042066eba4c531932bc11e3eec8
Tomasz <tomaszp@man.poznan.pl>
2018-01-08 14:24:24 +0100

    Fix testing

--- src/test/java/pl/put/poznan/foxtrot/logic/GraphTest.java
+++ src/test/java/pl/put/poznan/foxtrot/logic/GraphTest.java
@@ -17,6 +17,8 @@ class GraphTest {
     private List<Node> nodeList;
     private List<Connection> connectionList;
 
+    GraphTest() {}
+
     @Before
     public void setUp() {
         nodeList = new ArrayList<>();
